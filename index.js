b9900464bc8819c03006f666a11f3948753e6f8a
Yuichiro <yuixich@gmail.com>
2018-08-08 22:51:38 -0700

    adding more draft work

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 
 
+// emit error?
 class Queque {
 
     constructor() {
         this.pollingInterval = 1000;
+        this.namespace = 'namespace';
+        this.handler = () => {};
+
+        this.timer = null;
     }
 
     push() {
+    }
+
+    start() {
+        this.timer = setInterval(() => {
+            this.poll();
+        }, this.pollingInterval);
+    }
+
+    stop() {
+    }
+
+    poll() {
     }
 }
 
