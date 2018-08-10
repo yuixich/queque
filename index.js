dc090ab4fc71cd0e7a704c200e6254c7aba879c1
Yuichiro <yuixich@gmail.com>
2018-08-09 17:41:19 -0700

    WIP - adding more basic features

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
+'use strict';
 
+const Aigle = require('aigle');
+const redis = require('redis');
+Aigle.promisifyAll(redis);
+
+
+const POLL_SCRIPT = `
+local task = redis.call('ZRANGEBYSCORE', KEYS[1], '-inf', ARGV[1], 'LIMIT', 0, 1)[1]
+if (task) then
+  redis.call('ZREM', KEYS[1], task)
+end
+return task
+`;
 
 // emit error?
 class Queque {
 
-    constructor() {
-        this.pollingInterval = 1000;
+    constructor(config) {
+        this.pollingInterval = config.pollingInterval || 1000;
         this.namespace = 'namespace';
         this.handler = () => {};
 
         this.timer = null;
-        this.redis = Redis.createInstance();
+        this.pollScriptSha = null;
+        this.redis = redis.createClient(config.redis);
+    }
+
+    getMainTaskQueueKeyname() {
+        return `${this.namespace}:taskQueueMain`;
+    }
+
+    async initialize() {
+        console.log('loading script');
+        const resp = await this.redis.scriptAsync('load', POLL_SCRIPT);
+        console.log(resp);
+        this.pollScriptSha = resp;
     }
 
     push(jobKey, runAt, jsonable) {
     }
 
     start() {
+        console.log(`Start polling for new task`);
         this.timer = setInterval(() => {
-            this.poll();
+            this.poll()
+            .catch(console.log);
         }, this.pollingInterval);
     }
 
@@ -26,23 +53,43 @@ class Queque {
         this.timer = null;
     }
 
-    poll() {
-        this.redis.zrange...
+    async poll() {
+        const checkTime = Date.now();
+        for(let = 0; i < 100; i++) {
+            const taskKey = await this.redis.evalshaAsync(this.pollScriptSha, 1, this.getMainTaskQueueKeyname(), checkTime);
+            console.log(taskKey);
+
+            if (!taskKey) {
+                // no more task for now
+                return;
+            }
+
+            this.handle(taskKey);
+        }
     }
 
     handle() {
+        this.redis.getAsync(
     }
 }
 
 
-const qq = new Queque({
-    pollingInterval: 1000,
-    handler: async (job) => {
-        return;
-    },
-});
+async function main() {
+    const qq = new Queque({ pollingInterval: 1000,
+        handler: async (job) => {
+            return;
+        },
+        redis: {
+            host: '127.0.0.1',
+            port: '6379',
+            db: 0,
+            prefix: 'q',
+        },
+    });
 
-qq.push({
+    await qq.initialize();
+    qq.start();
+}
 
-});
+main().catch(console.log);
 
